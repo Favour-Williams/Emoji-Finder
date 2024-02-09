fea070f265da9f572caf4de310957aeba17e447e
williamsfavour <williamsfavour012345@gmail.com>
2024-02-09 23:42:56 +0200

    first commit

--- components/Emo.jsx
+++ components/Emo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Emo({emoji}) {
   return (
     <div >
-        <div> 
+        <div className='div2'> 
             {emoji.symbol} {emoji.title}
             
         </div>
